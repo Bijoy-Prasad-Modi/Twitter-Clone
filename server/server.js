be70a546cbf3136807f24f2f8f1c2a07502deca9
Bijoy Prasad Modi <bpmodi88@gmail.com>
2025-02-06 19:43:08 +0530

    JWT token generation, Signup function and User Schema has been being setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/authRoutes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.use("/api/auth", authRoutes);
 
