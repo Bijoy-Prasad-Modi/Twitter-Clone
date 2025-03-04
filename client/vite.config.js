8fa0881f01162b0aa02c67b69ceb9cf6b6f64958
Bijoy Prasad Modi <bpmodi88@gmail.com>
2025-03-04 18:14:49 +0530

    vite.Config.js update

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
+// export default defineConfig({
+//   plugins: [tailwindcss(), react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: process.env.VITE_BASE_URL,
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  console.log("Backend URL:", env.VITE_BASE_URL);
+  return {
+    plugins: [tailwindcss(), react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
