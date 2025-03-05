f59f8f5891ec64e021c34fe95327317927ffde17
Bijoy Prasad Modi <bpmodi88@gmail.com>
2025-03-05 08:17:06 +0530

    Base Url updated

--- client/src/hooks/useUpdateUserProfile.jsx
+++ client/src/hooks/useUpdateUserProfile.jsx
@@ -8,7 +8,7 @@ const useUpdateUserProfile = () => {
     useMutation({
       mutationFn: async (formData) => {
         try {
-          const res = await fetch(`/api/users/update`, {
+          const res = await fetch(`https://twitter-clone-c8hr.onrender.com/api/users/update`, {
             method: "POST",
             headers: {
               "Content-Type": "application/json",
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
@@ -23,13 +23,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://twitter-clone-c8hr.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://twitter-clone-c8hr.onrender.com",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
