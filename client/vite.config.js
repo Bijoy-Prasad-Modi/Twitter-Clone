22272264ee3f98242facbab31444c64f3ac71893
Bijoy Prasad Modi <bpmodi88@gmail.com>
2025-03-05 08:00:11 +0530

    viteConfig updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,34 +2,34 @@ import { defineConfig, loadEnv } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), "");
 
-  console.log("Backend URL:", env.VITE_BASE_URL);
-  return {
-    plugins: [tailwindcss(), react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_BASE_URL,
-          changeOrigin: true,
-          secure: false,
-        },
-      },
-    },
-  };
-});
-
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [tailwindcss(), react()],
-//   server: {
-//     proxy: {
-//       "/api": {
-//         target: process.env.VITE_BASE_URL,
-//         changeOrigin: true,
-//         secure: false,
+//   console.log("Backend URL:", env.VITE_BASE_URL);
+//   return {
+//     plugins: [tailwindcss(), react()],
+//     server: {
+//       proxy: {
+//         "/api": {
+//           target: env.VITE_BASE_URL,
+//           changeOrigin: true,
+//           secure: false,
+//         },
 //       },
 //     },
-//   },
+//   };
 // });
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://twitter-clone-c8hr.onrender.com",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
